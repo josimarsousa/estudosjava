91898f1519a5c1e0873ba57a0fd5586ab840621f
josimar <desktop-casa>
2023-01-11 20:28:15 -0300

    seters e getters criados

--- projeto/src/projeto/classes/Cliente.java
+++ projeto/src/projeto/classes/Cliente.java
@@ -9,5 +9,48 @@ public class Cliente {
 	private String cpf;
 	private String cnpj;
 	private String obs;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getFone() {
+		return fone;
+	}
+	public void setFone(String fone) {
+		this.fone = fone;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getObs() {
+		return obs;
+	}
+	public void setObs(String obs) {
+		this.obs = obs;
+	}
 
+	
 }
