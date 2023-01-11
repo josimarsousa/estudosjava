9ecb6d8d523035a72f0c4033abb00083eba0967c
josimar <desktop-casa>
2023-01-11 20:25:37 -0300

    classe cliente atualizada

--- projeto/src/projeto/classes/Cliente.java
+++ projeto/src/projeto/classes/Cliente.java
@@ -1,5 +1,13 @@
 package projeto.classes;
 
 public class Cliente {
+	
+	private int id;
+	private String nome;
+	private String endereco;
+	private String fone;
+	private String cpf;
+	private String cnpj;
+	private String obs;
 
 }
